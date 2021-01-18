1ac97030191e0ea7ee992af787b1e7aba4942a1b
Jaden Rosoff <rosoffj8349@my.sduhsd.net>
2021-01-18 01:45:53 +0000

    edits

--- proj/student.c
+++ proj/student.c
@@ -49,12 +49,48 @@ void saveStudents(int key)
   //       tom thumb 15 1234 
   //       james dean 21 2345 
   //       katy jones 18 4532 
+  FILE* fp;
+
+  fp = fopen(STUFILE, "rw");
+  if (fp)
+  {
+    for (int i = 0; i < numStudents, i++)
+    {
+//strcpy parts of students into string
+      char * line[256] = students[i]->firstName, students[i]->lastName, students[i]->age, students[i]->id;
+      strcpy(line, students[i]->firstName);
+      strcat(line, students[i]->lastName);
+      strcat(line, students[i]->age);
+      strcat(line, students[i]->id);
+//opposite of load
+      caesarDecrypt(line, key);
+      fprint("%s", line);
+    }
+  }
+  fclose(fp);
 }
 
 
 void loadStudents(int key)
 {
   // load the students from the data file overwriting all exisiting students in memory
+  FILE* fp;
+  char line[256];
+  fp = fopen(STUFILE, "rw");
+  if (fp)
+  {
+    while (fgets(line, sizeof(line), fp))
+    {
+      //might be decrypt
+      caesarEncrypt(line, key);
+      char * fname = strtok(line, " ");
+      char * lname = strtok(NULL, " ");
+      int age = atoi(strtok(NULL, " "));
+      int id = atoi(strtok(NULL, " "));
+      createStudent(fname, lname, age, id);
+    }
+  }
+  fclose(fp);
 }
 
 
